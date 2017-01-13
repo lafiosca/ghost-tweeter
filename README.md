0ec25dd9d0d4d500004d2b701458df3edcc6b6ba
Joe Lafiosca <joe@lafiosca.com>
2017-01-12 23:45:50 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+ghost-tweeter
+=============
+
+A scheduled Lambda function for tweeting links to Ghost blog entries. (Currently only works with Ghost blogs backed by Postgres databases.)
---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const Promise = require('bluebird');
+const Twit = require('twit');
+const pgp = require('pg-promise')({
+	promiseLib: Promise,
+});
+const promiserToLambda = require('promiser-to-lambda');
+
+const db = pgp({
+	host: process.env.DATABASE_HOST,
+	port: process.env.DATABASE_PORT || 5432,
+	database: process.env.DATABASE_NAME,
+	user: process.env.DATABASE_USER,
+	password: process.env.DATABASE_PASSWORD,
+});
+
+const twitter = new Twit({
+	consumer_key: process.env.TWITTER_CONSUMER_KEY,
+	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+	access_token: process.env.TWITTER_ACCESS_TOKEN,
+	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
+
+exports.handler = promiserToLambda(event => {
+	return db.oneOrNone('select title,slug from posts where published_at < now() order by published_at desc limit 1')
+		.then(latestPost => {
+			console.log('latest post:', latestPost);
+		});
+});
